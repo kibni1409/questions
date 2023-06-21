a497754ccbdbc4d79c5422963e44abcd304cf814
Andrey <kibni1409@gmail.com>
2023-06-21 17:39:02 +0300

    Настройка соединения

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-import './ws.js'
+import Style from './App.module.css';
+import {useRef, useState} from "react";
+import {v4} from "uuid";
+
+// Создает сокет
+const socket = new WebSocket('ws://localhost:8081');
+
+// Открываем событие на открытие соединения
+socket.addEventListener('open', () => {
+    console.log('open')
+});
+
+// Создаем собитые на закрытие соединеня
+socket.addEventListener('close', () => {
+    console.log('The connection has been closed');
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+    let refInput = useRef()
+
+
+    // Отправляем сообщение на сервер
+    function SendMessage(e) {
+        e.preventDefault()
+        let obj = {
+            name,
+            message: 'joined the chat',
+            [refInput.current.id]: refInput.current.value
+        }
+        if(refInput.current.id === 'name') setName(refInput.current.value)
+        socket.send(JSON.stringify(obj))
+    }
+
+    // Создаем событие на получение сообщения
+    socket.addEventListener('message', (event) => {
+        let res = JSON.parse(event.data)
+        if (res.messages) setMessages([...res.messages])
+    });
+
+    return (
+        <div className={Style.App}>
+            <header className={Style.App_header}>
+                <div id={'chat'}>
+                    {messages.map((mes) =>
+                        <p className={mes.name === name
+                            ? Style.myMess
+                            : Style.anyMess}
+                           key={v4()}>[<i>{mes.name}</i>]: {mes.message}</p>)}
+                </div>
+                <form onSubmit={SendMessage}>
+                    {name === ''
+                        ? <input ref={refInput} id='name' placeholder='name'/>
+                        : <input ref={refInput} id='message' placeholder='message'/>}
+                    <button type={"submit"}>Send</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
